3fbe3d606bd94a07029f813c88557f5427975b8e
Kirjia <40699415+Kirjia@users.noreply.github.com>
2024-11-07 15:11:52 +0100

    inserimento prodtto da pagina item

---
+++ src/main/webapp/js/item.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+	
+	$("#addToCart").on("click", function(event){
+			event.preventDefault(); 
+
+			
+			
+			var skuValue = $("#sku").val();
+			var quantityValue = $("#quantity").val();
+
+			
+			$.ajax({
+				url: "AddToCart",
+				method: "POST",
+				
+				data: {
+					sku: skuValue,
+					quantity: quantityValue
+				},
+				
+				success: function(data){
+					var jsonData = data.response[0];
+					if(jsonData){
+						alert("Prodotto aggiunto al carrello!");
+					}else{
+					alert("Si è verificato un errore durante l'aggiunta al carrello.");
+					}
+                },
+				error: function(xhr, status, error){
+					alert("Si è verificato un errore durante l'aggiunta al carrello " + error);
+				}
+						
+			});
+				
+				
+		})
+	
+})
+
